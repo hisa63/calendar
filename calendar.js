f1d444cf98027cc336c2b68e587c04028199e1b2
hisa <hisayuki.k8@gmail.com>
2020-11-19 08:12:23 +0900

    [updata]

--- calendar.js
+++ calendar.js
@@ -1,9 +1,20 @@
 document.getElementById('calendar').addEventListener('click', function(e) {
     let mouseX = e.clientX;
     let mouseY = e.clientY;
-    // window.alert(mouseX + ":" + mouseY);
     
     const newdiv = document.createElement('div');
     newdiv.innerHTML = 'new event';
+
+    let i = 1;
+    let id = "new-event-";
+
+    newdiv.classList.add(id + i);
+    i++;
     document.getElementById('calendar').appendChild(newdiv);
+});
+
+document.getElementsByClassName('new-event-1').addEventListener('click', function() {
+    let event = document.getElementsByClassName('new-event-1');
+
+    event[0].style.backgroundColor=red;
 });
