1e67d8654cc03056660e6ca5cbfd4385d7edb5d7
hisa <hisayuki.k8@gmail.com>
2020-11-19 09:43:21 +0900

    [updata] 付加属性class -> id

--- calendar.js
+++ calendar.js
@@ -1,20 +1,31 @@
-document.getElementById('calendar').addEventListener('click', function(e) {
-    let mouseX = e.clientX;
-    let mouseY = e.clientY;
+// document.getElementById('calendar').addEventListener('click', function(e) {
+//     let mouseX = e.clientX;
+//     let mouseY = e.clientY;
     
+//     const newdiv = document.createElement('div');
+//     newdiv.innerHTML = 'new event';
+
+//     let i = 1;
+//     let id = "new-event-";
+
+//     newdiv.classList.add(id + i);
+//     i++;
+//     newdiv.style.top = mouseY;
+//     newdiv.style.left = mouseX;
+//     document.getElementById('calendar').appendChild(newdiv);
+// });
+
+
+
+
+document.getElementById('calendar').addEventListener('click', function(e) {
     const newdiv = document.createElement('div');
     newdiv.innerHTML = 'new event';
 
     let i = 1;
     let id = "new-event-";
 
-    newdiv.classList.add(id + i);
+    newdiv.setAttribute("id", id + i);
     i++;
     document.getElementById('calendar').appendChild(newdiv);
-});
-
-document.getElementsByClassName('new-event-1').addEventListener('click', function() {
-    let event = document.getElementsByClassName('new-event-1');
-
-    event[0].style.backgroundColor=red;
 });
